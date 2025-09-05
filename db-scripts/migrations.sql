6b335476bdf3140a9b20d45d99e524d4a2feb0ac
hatim <azizihatim11@gmail.com>
2025-09-05 14:56:00 +0100

    fix: add existence checks to migration script for idempotent execution

--- db-scripts/migrations.sql
+++ db-scripts/migrations.sql
@@ -9,31 +9,50 @@ END;
 GO
 
 BEGIN TRANSACTION;
-CREATE TABLE [Appointments] (
-    [Id] uniqueidentifier NOT NULL,
-    [CustomerId] uniqueidentifier NOT NULL,
-    [StartUtc] datetime2 NOT NULL,
-    [EndUtc] datetime2 NOT NULL,
-    [Status] nvarchar(max) NOT NULL,
-    CONSTRAINT [PK_Appointments] PRIMARY KEY ([Id])
-);
 
-CREATE TABLE [Customers] (
-    [Id] uniqueidentifier NOT NULL,
-    [Name] nvarchar(100) NOT NULL,
-    [Email] nvarchar(200) NOT NULL,
-    CONSTRAINT [PK_Customers] PRIMARY KEY ([Id])
-);
+IF OBJECT_ID(N'[Appointments]') IS NULL
+BEGIN
+    CREATE TABLE [Appointments] (
+        [Id] uniqueidentifier NOT NULL,
+        [CustomerId] uniqueidentifier NOT NULL,
+        [StartUtc] datetime2 NOT NULL,
+        [EndUtc] datetime2 NOT NULL,
+        [Status] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Appointments] PRIMARY KEY ([Id])
+    );
+END;
 
-CREATE INDEX [IX_Appointments_StartUtc_EndUtc] ON [Appointments] ([StartUtc], [EndUtc]);
+IF OBJECT_ID(N'[Customers]') IS NULL
+BEGIN
+    CREATE TABLE [Customers] (
+        [Id] uniqueidentifier NOT NULL,
+        [Name] nvarchar(100) NOT NULL,
+        [Email] nvarchar(200) NOT NULL,
+        CONSTRAINT [PK_Customers] PRIMARY KEY ([Id])
+    );
+END;
 
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20250904010338_Init', N'9.0.8');
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_Appointments_StartUtc_EndUtc' AND object_id = OBJECT_ID('Appointments'))
+BEGIN
+    CREATE INDEX [IX_Appointments_StartUtc_EndUtc] ON [Appointments] ([StartUtc], [EndUtc]);
+END;
 
-ALTER TABLE [Appointments] ADD [Notes] nvarchar(1000) NULL;
+IF NOT EXISTS (SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20250904010338_Init')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250904010338_Init', N'9.0.8');
+END;
 
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20250905013814_appointment-notes', N'9.0.8');
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('Appointments') AND name = 'Notes')
+BEGIN
+    ALTER TABLE [Appointments] ADD [Notes] nvarchar(1000) NULL;
+END;
+
+IF NOT EXISTS (SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20250905013814_appointment-notes')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250905013814_appointment-notes', N'9.0.8');
+END;
 
 COMMIT;
 GO
