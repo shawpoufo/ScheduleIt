d9524b529cf2e7b6c465c5f2e48849418ffd8a32
hatim <azizihatim11@gmail.com>
2025-09-05 08:51:28 +0100

    feat: add appointment notes field and update database script migration
    - Add Notes column to Appointments table

---
+++ db-scripts/migrations.sql
@@ -0,0 +1,40 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Appointments] (
+    [Id] uniqueidentifier NOT NULL,
+    [CustomerId] uniqueidentifier NOT NULL,
+    [StartUtc] datetime2 NOT NULL,
+    [EndUtc] datetime2 NOT NULL,
+    [Status] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Appointments] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Customers] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(100) NOT NULL,
+    [Email] nvarchar(200) NOT NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY ([Id])
+);
+
+CREATE INDEX [IX_Appointments_StartUtc_EndUtc] ON [Appointments] ([StartUtc], [EndUtc]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250904010338_Init', N'9.0.8');
+
+ALTER TABLE [Appointments] ADD [Notes] nvarchar(1000) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250905013814_appointment-notes', N'9.0.8');
+
+COMMIT;
+GO
+
